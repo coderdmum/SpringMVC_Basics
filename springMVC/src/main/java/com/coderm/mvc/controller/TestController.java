afdcdc5e39b7722c237c9ca5c4dae8140c4a1870
coderm <coderm@163.com>
2021-11-25 21:06:25 +0800

    2021-11-25-2106

--- springMVC/src/main/java/com/coderm/mvc/controller/TestController.java
+++ springMVC/src/main/java/com/coderm/mvc/controller/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
 
     @RequestMapping("/")
-    public String indec(){
+    public String index(){
         return "index";
     }
 }
