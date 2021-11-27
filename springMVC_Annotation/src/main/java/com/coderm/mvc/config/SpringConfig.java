26fa25e59dc04685e4bf6b124b651cd7e16262bc
coderm <coderm@163.com>
2021-11-27 20:46:06 +0800

    2021-11-27-2045

---
+++ springMVC_Annotation/src/main/java/com/coderm/mvc/config/SpringConfig.java
@@ -0,0 +1,4 @@
+package com.coderm.mvc.config;
+
+public class SpringConfig {
+}
---
+++ springMVC_demo4/src/main/java/com/coderm/mvc/interceptors/FirstInterceptor.java
@@ -0,0 +1,37 @@
+package com.coderm.mvc.interceptors;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Date:2021/7/10
+ * Author:ybc
+ * Description:
+ */
+@Component
+public class FirstInterceptor implements HandlerInterceptor {
+
+    //在控制方法执行之前执行
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("FirstInterceptor-->preHandle");
+        return true;
+    }
+
+    //在控制方法执行之后执行
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("FirstInterceptor-->postHandle");
+    }
+
+    //在视图渲染完毕之后执行
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("FirstInterceptor-->afterCompletion");
+    }
+}
